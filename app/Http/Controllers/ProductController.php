ccc3126bab4d9403c52e24a2d6a30e9826ae07de
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-09 22:57:15 -0500

    Do not paginate product list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-      return ProductResource::collection(Product::paginate(25));
+      return ProductResource::collection(Product::all());
     }
 
     /**
