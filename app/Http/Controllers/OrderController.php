1fb96d05e38c1c142251774b088106eb116aa8f3
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-10 02:56:14 -0500

    Add Order controller

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use App\Http\Resources\OrderResource;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        OrderResource::collection(Order::paginate(25))
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $order = Order::create([
+        'product_bundle_id' => $request->product_bundle_id,
+        'quantity' => $request->quantity,
+      ]);
+
+      return new OrderResource($order);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $order = Order::find($id);
+      return new OrderResource($order);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $order = Order::find($id);
+      $order->update($request->only([
+        'product_bundle_id',
+        'quantity',
+      ]));
+
+      return new OrderResource($order);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $order = Order::find($id);
+      $order->delete();
+
+      return response()->json(null, 204);
+    }
+}
